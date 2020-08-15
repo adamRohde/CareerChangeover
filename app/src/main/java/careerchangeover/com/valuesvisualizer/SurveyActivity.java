ec86321f81d65fb2224e42a2ba8782b2a9fc207a
Jignesh <jpatel@jadeworld.com>
2020-08-16 09:22:46 +1200

    Values Screen

--- app/src/main/java/careerchangeover/com/valuesvisualizer/SurveyActivity.java
+++ app/src/main/java/careerchangeover/com/valuesvisualizer/SurveyActivity.java
@@ -2,10 +2,19 @@ package careerchangeover.com.valuesvisualizer;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.preference.PreferenceManager;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -19,6 +28,10 @@ public class SurveyActivity extends AppCompatActivity implements View.OnClickLis
     ListView listView;
     Value[] valuesArray;
     String column;
+    AlertDialog mAlertDialog;
+
+    public static final int REQUEST_FIRST_INTENT = 10;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,21 +81,66 @@ public class SurveyActivity extends AppCompatActivity implements View.OnClickLis
                 mPref = PreferenceManager.getDefaultSharedPreferences(this);
                 boolean doNotShowAgain = mPref.getBoolean(disclaimerAcceptedPref,false);
 
-                // Decides whether or not to view disclaimer
-                if (column.startsWith("employer") || doNotShowAgain) {
-                    Intent save = new Intent(SurveyActivity.this,MainActivity.class);
-                    startActivity(save);
-                } else {
-                    Intent disclaimerPopUp = new Intent(SurveyActivity.this,DisclaimerPopUp.class);
-                    startActivity(disclaimerPopUp);
-                }
+//                // Decides whether or not to view disclaimer
+//                if (column.startsWith("employer") || doNotShowAgain) {
+//                    Intent save = new Intent(SurveyActivity.this,MainActivity.class);
+//                    startActivity(save);
+//                } else {
+//                    Intent disclaimerPopUp = new Intent(SurveyActivity.this,DisclaimerPopUp.class);
+//                    startActivity(disclaimerPopUp);
+//                }
+
                 // Calculates results if survey is completed
                 if (checkSurveyComplete()) {
                     myDbHandler.calculateScore(valuesArray,column);
+                    Intent mainActivityIntent = new Intent(SurveyActivity.this, MainActivity.class);
+                    startActivity(mainActivityIntent);
+                }else
+                {
+                    setReminder();
                 }
 
                 break;
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // if this is true, this means your first intent finished. So you can start your second intent
+        if (requestCode == REQUEST_FIRST_INTENT) {
+            Intent mainActivityIntent = new Intent(SurveyActivity.this, MainActivity.class);
+            startActivity(mainActivityIntent);
+        }
+    }
+
+    private void setReminder() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(SurveyActivity.this);
+        View view = LayoutInflater.from(SurveyActivity.this).inflate(R.layout.set_reminder_layout, null);
+        view.findViewById(R.id.set_reminder_yes).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAlertDialog.hide();
+                Intent insertCalendarIntent = new Intent(Intent.ACTION_INSERT)
+                        .setData(CalendarContract.Events.CONTENT_URI)
+                        .putExtra(CalendarContract.Events.TITLE, "Complete Values Survey");
+                startActivityForResult(insertCalendarIntent, REQUEST_FIRST_INTENT);
+
+            }
+        });
+        view.findViewById(R.id.set_reminder_no).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAlertDialog.hide();
+                Intent mainActivityIntent = new Intent(SurveyActivity.this, MainActivity.class);
+                startActivity(mainActivityIntent);
+            }
+        });
+        builder.setView(view);
+
+        mAlertDialog = builder.create();
+        mAlertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        mAlertDialog.show();
     }
 
     private boolean checkSurveyComplete() {
